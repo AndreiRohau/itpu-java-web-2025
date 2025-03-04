daf5118d09de90856677352d14683fd95775c07c
Andrei Rohau <arohau@edmunds.com>
2025-03-04 17:50:04 +0700

    ws-11-servlets-and-jsp

--- ws-11-servlets-and-jsp/src/main/java/com/arohau/FrontController.java
+++ ws-11-servlets-and-jsp/src/main/java/com/arohau/FrontController.java
@@ -11,6 +11,7 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("FrontController.doGet()");
+        System.out.println("new version...");
         req.getRequestDispatcher("home.jsp").forward(req, resp);
     }
 }
