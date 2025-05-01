eeba0049cc3ff2d2f3ae6556f8b7cdd9db283d77
Andrei Rohau <arohau@edmunds.com>
2025-05-01 07:19:55 +0300

    ws-24-base

---
+++ ws-24-spring-security/oauth2-based-auth/src/test/java/com/arohau/oauth2basedauth/data/repository/AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arohau.oauth2basedauth.data.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
