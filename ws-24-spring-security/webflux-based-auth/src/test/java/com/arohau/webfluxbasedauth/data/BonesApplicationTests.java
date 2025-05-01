4cfcd24666d0db8c568e792f4c4f1c9c840b120d
Andrei Rohau <arohau@edmunds.com>
2025-05-01 06:53:57 +0300

    ws-24-base

---
+++ ws-24-spring-security/webflux-based-auth/src/test/java/com/arohau/webfluxbasedauth/data/BonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arohau.webfluxbasedauth.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
